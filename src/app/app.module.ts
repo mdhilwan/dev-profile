d62d84383d045751ba713498c84eed2219220cba
Md Hilwan <wan.wanted.1@gmail.com>
2021-08-05 14:12:01 -0500

    split into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './components/main-header/main-header.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
