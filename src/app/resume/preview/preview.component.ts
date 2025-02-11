bca10c2daf10522b44ee323a1365bfd572e30528
Md Hilwan <wan.wanted.1@gmail.com>
2025-02-10 21:45:37 -0600

    changes to preview

--- src/app/resume/preview/preview.component.ts
+++ src/app/resume/preview/preview.component.ts
@@ -1,25 +1,49 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy } from '@angular/core';
 import { PageDataService } from '../../shared/services/page-data.service';
-import { AuthService } from '../../shared/services/auth.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
   styleUrls: ['./preview.component.scss']
 })
-export class PreviewComponent {
+export class PreviewComponent implements OnDestroy {
 
   public basicInfo: any = {};
   public employmentHistory: any = [];
   public date: Date = new Date();
+  public isPrinting = false;
 
-  constructor(private http: HttpClient,
-              private pageStateService: PageDataService,
-              public authService: AuthService) {
+  private destroyed$ = new Subject<void>();
+
+  constructor(private pageStateService: PageDataService) {
     this.pageStateService.GetBasicInfo().subscribe((data: any) => this.basicInfo = data);
     this.pageStateService.GetEmploymentHistory().subscribe((data: any) => this.employmentHistory = data);
+    window.addEventListener('beforeprint', () => this.onBeforePrint());
+    window.addEventListener('afterprint', () => this.onAfterPrint());
     setInterval(() => this.date = new Date(), 1000);
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+    window.removeEventListener('beforeprint', () => this.onBeforePrint());
+    window.removeEventListener('afterprint', () => this.onAfterPrint());
+  }
+
+  onBeforePrint(): void {
+    this.isPrinting = true;
+  }
+
+  onAfterPrint(): void {
+    this.isPrinting = false;
+  }
+
+  getContainer(): string {
+    if (this.isPrinting) {
+      return 'container-fluid px-5';
+    }
+    return 'container';
   }
 
 }
